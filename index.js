872fec33a4c14ae40ec108fe5aac93ab597777c9
unow0517 <unow1992@gmail.com>
2021-01-19 21:29:28 +0100

    put all the GIS image in

---
+++ index.js
@@ -0,0 +1,28 @@
+function gradWork(){
+  document.getElementById('gradWork').innerHTML = `<img src="./images/analysis.jpg" alt="analysis">
+  explain comes soon`
+  document.getElementById('gradWork').classList.toggle('show')
+}
+
+function fourmaps(){
+  document.getElementById('4maps').innerHTML = "explain comes soon"
+  document.getElementById('4maps').classList.toggle('show')
+}
+
+function sixmaps(){
+  document.getElementById('sixmaps').innerHTML = "explain comes soon"
+  document.getElementById('sixmaps').classList.toggle('show')
+}
+
+function finalmaps(){
+  document.getElementById('finalmaps').innerHTML = `<img class='conceptdesign' src="./images/conceptDesign.jpg" alt="analysis">
+  explain comes soon`
+  document.getElementById('finalmaps').classList.toggle('show')
+}
+
+function naturpark(){
+  document.getElementById('naturpark').innerHTML = `<img class='conceptdesign' src="./images/panel.jpg" alt="analysis">
+  explain comes soon`
+  document.getElementById('naturpark').classList.toggle('show')
+}
+
